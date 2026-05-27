946bca91000e7b05fb8c2135211257eb7c1a4084
aldiyanto211065-maks <aldiyanto211065@gmail.com>
2026-05-27 23:09:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
 import streamlit as st
 from data_utils import load_data, preprocess_data, compute_similarity
-import home, about, preference, recommendation, contact
+
+import home
+import about
+import preference
+import recommendation
+import contact
+
+st.set_page_config(
+    page_title="Skincare Recommendation System",
+    page_icon="✨",
+    layout="wide"
+)
 
 def main():
-    with st.spinner("📂 Memuat dan memproses data..."):
-        df = load_data()
-        df = preprocess_data(df)
-        similarity_matrix = compute_similarity(df)
 
-    st.sidebar.title("Menu")
-    page = st.sidebar.radio("Pilih Halaman:", ["Home", "Preference", "Recommendation Result", "About", "Contact"])
+    try:
 
-    if page == "Home":
-        home.app()
-    elif page == "Preference":
-        preference.app(df)
-    elif page == "Recommendation Result":
-        recommendation.app(df, similarity_matrix)
-    elif page == "About":
-        about.app()
-    elif page == "Contact":
-        contact.app()
+        with st.spinner("📂 Memuat dan memproses data..."):
+
+            df = load_data()
+            df = preprocess_data(df)
+            similarity_matrix = compute_similarity(df)
+
+        st.sidebar.title("Menu")
+
+        page = st.sidebar.radio(
+            "Pilih Halaman:",
+            [
+                "Home",
+                "Preference",
+                "Recommendation Result",
+                "About",
+                "Contact"
+            ]
+        )
+
+        if page == "Home":
+            home.app()
+
+        elif page == "Preference":
+            preference.app(df)
+
+        elif page == "Recommendation Result":
+            recommendation.app(df, similarity_matrix)
+
+        elif page == "About":
+            about.app()
+
+        elif page == "Contact":
+            contact.app()
+
+    except Exception as e:
+        st.error(f"Terjadi error: {e}")
 
 if __name__ == "__main__":
     main()
