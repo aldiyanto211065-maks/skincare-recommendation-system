35af18aa1099ec48da63beac9bfdb0498bfcbda5
aldiyanto211065-maks <aldiyanto211065@gmail.com>
2026-05-27 23:11:11 +0700

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,20 +1,10 @@
 import streamlit as st
 
 def app():
-    st.title("📬 Kontak Kami")
-    st.markdown("""
-    Jika Anda memiliki pertanyaan, saran, atau ingin berkolaborasi, silakan hubungi kami melalui formulir di bawah ini.
-    """)
 
-    with st.form(key='contact_form'):
-        name = st.text_input("Nama")
-        email = st.text_input("Email")
-        message = st.text_area("Pesan")
+    st.title("📞 Contact")
 
-        submit_button = st.form_submit_button(label='Kirim')
+    st.write("Developer:")
+    st.write("Mohamad Fiqih Aldiyanto")
 
-    if submit_button:
-        if not name or not email or not message:
-            st.error("Mohon isi semua kolom.")
-        else:
-            st.success("Terima kasih sudah menghubungi kami! Kami akan membalas secepatnya.")
+    st.write("Universitas Muhammadiyah Jember")
